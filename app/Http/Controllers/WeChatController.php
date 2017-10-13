cd8fb375028ca6df04f474d8711f2ee25d9689be
oryxtc <oryxtc@gmail.com>
2017-10-13 16:59:54 +0800

    新增wechat包

--- app/Http/Controllers/WeChatController.php
+++ app/Http/Controllers/WeChatController.php
@@ -10,7 +10,7 @@ class WeChatController extends Controller
     public function __construct()
     {
         $this->middleware('web');
-        //$this->middleware('wechat.oauth');
+        $this->middleware('wechat.oauth');
     }
 
     /**
@@ -20,7 +20,6 @@ class WeChatController extends Controller
      */
     public function serve()
     {
-
         $app = app('wechat.official_account');
         $app->server->push(function($message){
             return "欢迎关注 overtrue！";
